afbb85776176a3694edf46d100d8eca54a925044
AceyAdapter <nicholaskreitz@gmail.com>
2022-06-16 00:37:25 +0100

    nojekyll

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -1,9 +1,9 @@
 export const manifest = {
 	appDir: "_app",
-	assets: new Set(["favicon.png","img/daisyui.jpeg","img/sveltekit.png","img/vs.png"]),
+	assets: new Set([".nojekyll","favicon.png","img/daisyui.jpeg","img/sveltekit.png","img/vs.png"]),
 	mimeTypes: {".png":"image/png",".jpeg":"image/jpeg"},
 	_: {
-		entry: {"file":"start-1c0b2e4f.js","js":["start-1c0b2e4f.js","chunks/index-b6926417.js"],"css":[]},
+		entry: {"file":"start-124c161b.js","js":["start-124c161b.js","chunks/index-b6926417.js"],"css":[]},
 		nodes: [
 			() => import('./nodes/0.js'),
 			() => import('./nodes/1.js'),
